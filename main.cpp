68d9d28cc9c760f7dddc012df435874ac843cac6
frakod <frakod@pm.me>
2021-11-30 05:16:57 +0000

    hell

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+#include <csignal> 
 #include <stdio.h>
 #include <unistd.h>
 #include "SnekGame.hpp"
 
+void InterruptHandler(int signum)
+{
+	puts("\n\e[33;2m >> Interrupt Recieved, Exiting... \e[0m");
+	exit(signum);
+}
+
 int main()
 {
+	std::signal(SIGINT, InterruptHandler);
 	char key = 'X';
 	SnekGame3D<> game(12,12,12);
 	
@@ -14,13 +22,14 @@ int main()
 		{
 			// display game.snek points as white
 			// and game.food point as red color
-			puts("running..");
+			printf("running.. %d %d %d\n",
+			game.snek[0].x, game.snek[0].y, game.snek[0].z);
 		}
 		else
 		{
 			// game over
 			puts("\n\e[38;2mgame over!");
 		}
-		usleep(10000);
+		usleep(500000);
 	}
 }
